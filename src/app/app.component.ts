0534662212877f0d61b532f9c80153ed72cb662d
swatitiwari5300 <swatitiwari5300@gmail.com>
2025-05-15 23:52:36 +0530

    angular tut'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
+import { ProfileComponent } from './profile/profile.component';
 
 @Component({
   selector: 'app-root',
-  imports: [LoginComponent, SignupComponent],
+  imports: [LoginComponent, SignupComponent, ProfileComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -14,4 +15,8 @@ export class AppComponent {
   name = 'Swati';
   x=10;
   y=20;
+
+  handleClickEvent() {
+    alert("Function called!!!!")
+  }
 }
