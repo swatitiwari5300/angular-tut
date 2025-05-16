f1ebed6e3039a64a828bf55bdb77eaa16ff530ed
swatitiwari5300 <swatitiwari5300@gmail.com>
2025-05-16 20:56:23 +0530

    angular tut

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core';
   selector: 'app-login',
   imports: [],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css', './login.btn.css'],
 })
 export class LoginComponent {
  login = "loginworks"
